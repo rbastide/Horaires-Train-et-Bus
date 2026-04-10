9a2bc5d60a86b129e125916026a61b8eaa31df81
Remi BASTIDE <r.bastide@grandperigueux.fr>
2026-04-10 16:01:33 +0200

    Refacto des noms des fichiers

--- src/Scripts/Loaders/busSectionLoad.js
+++ src/Scripts/Loaders/busSectionLoad.js
@@ -1,11 +1,11 @@
 async function loadBusSection() {
     try {
-    const res = await fetch("Components/SectionBus.html");
+    const res = await fetch("Components/FirstSectionBus.html");
     if (!res.ok) throw new Error("HTTP " + res.status);
     const html = await res.text();
     document.getElementById("bus-section-placeholder").innerHTML = html;
   } catch (err) {
-    console.error("Erreur chargement SectionBus.html :", err);
+    console.error("Erreur chargement FirstSectionBus.html :", err);
   }
     
 }
--- src/Scripts/Loaders/trainSectionLoad.js
+++ src/Scripts/Loaders/trainSectionLoad.js
@@ -1,11 +1,11 @@
 async function loadTrainSection() {
     try {
-    const res = await fetch("Components/SectionTrain.html");
+    const res = await fetch("Components/FirstSectionTrain.html");
     if (!res.ok) throw new Error("HTTP " + res.status);
     const html = await res.text();
     document.getElementById("train-section-placeholder").innerHTML = html;
   } catch (err) {
-    console.error("Erreur chargement SectionTrain.html :", err);
+    console.error("Erreur chargement FirstSectionTrain.html :", err);
   }
     
 }

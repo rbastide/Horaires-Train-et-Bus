2cb1946d92aa71c35b7cb40d18117e50d46efa34
Remi BASTIDE <r.bastide@grandperigueux.fr>
2026-05-06 10:54:15 +0200

    Fixbug d'un mauvais appel de l'heure

--- src/FrontEnd/Scripts/dateTime.js
+++ src/FrontEnd/Scripts/dateTime.js
@@ -1,10 +1,21 @@
 function updateDateTime() {
+            const dateTimeElement = document.getElementById('datetime');
+
+            if (!dateTimeElement) return;
+
             const now = new Date();
             const time = now.toLocaleTimeString('fr-FR');
-            document.getElementById('datetime').innerHTML = '<p>'  + time + '</p>';
+            document.getElementById('datetime').innerHTML = time;
         }
 
-        document.addEventListener('DOMContentLoaded', function(){
-            setInterval(updateDateTime, 1000);
+document.addEventListener('DOMContentLoaded', function(){
+
+    const interval = setInterval(() => {
+        if (document.getElementById('datetime')){
             updateDateTime();
-        })
+            setInterval(updateDateTime, 1000);
+            clearInterval(interval);
+        }
+    },100);
+            
+});

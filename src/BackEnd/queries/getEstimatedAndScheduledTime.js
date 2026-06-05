80a159ffb257eddd6b837f3338e9f0eedb5a583a
rbastide <remi.bastide@etu.unilim.fr>
2026-06-05 15:32:59 +0200

    Suppression d'un paramètre dans la clause ORDER BY

--- src/BackEnd/queries/getEstimatedAndScheduledTime.js
+++ src/BackEnd/queries/getEstimatedAndScheduledTime.js
@@ -2,6 +2,6 @@ import { query } from "../services/databaseConnection.js";
 import { getListStops } from "../Utils/helpers.js";
 
 export async function getEstimatedAndScheduledTime(localTime,stops){
-    const rows = await query(`SELECT DISTINCT pt.passing_time_scheduled, pt.passing_time_estimated, pt.stop_id FROM v_avl_rt_passing_times pt WHERE pt.stop_label IN (${getListStops(stops)}) AND pt.passing_time_scheduled >= "${localTime}" ORDER BY pt.passing_time_scheduled, pt.passing_time_estimated ASC;`);
+    const rows = await query(`SELECT DISTINCT pt.passing_time_scheduled, pt.passing_time_estimated, pt.stop_id FROM v_avl_rt_passing_times pt WHERE pt.stop_label IN (${getListStops(stops)}) AND pt.passing_time_scheduled >= "${localTime}" ORDER BY pt.passing_time_scheduled ASC;`);
     return rows || null;
 }

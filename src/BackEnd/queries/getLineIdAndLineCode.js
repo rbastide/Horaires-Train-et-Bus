cea0eba7dc390399f4547eae7c3c74da3c93681b
rbastide <remi.bastide@etu.unilim.fr>
2026-06-05 14:49:49 +0200

    Ajout d'une limite de 10 au requetes (pour optimisation lancement)

--- src/BackEnd/queries/getLineIdAndLineCode.js
+++ src/BackEnd/queries/getLineIdAndLineCode.js
@@ -2,6 +2,6 @@ import { query } from "../services/databaseConnection.js";
 import { getListStops } from "../Utils/helpers.js";
 
 export async function getLineIdAndLineCode(stops){
-    const rows = await query(`SELECT routes.line_id, routes.line_code, routes.link_stop_start_id FROM v_avl_rt_routes routes WHERE routes.link_stop_start_id IN (SELECT pt.stop_id FROM v_avl_rt_passing_times pt WHERE pt.stop_label IN (${getListStops(stops)}) GROUP BY pt.stop_id);`);
+    const rows = await query(`SELECT routes.line_id, routes.line_code, routes.link_stop_start_id FROM v_avl_rt_routes routes WHERE routes.link_stop_start_id IN (SELECT pt.stop_id FROM v_avl_rt_passing_times pt WHERE pt.stop_label IN (${getListStops(stops)}) GROUP BY pt.stop_id) LIMIT 10;`);
     return rows || null;
 }
